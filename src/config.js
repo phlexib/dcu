121559e72fa40fb564dca29571dbe7e422f9bc3d
Ben Rohel <b.rohel@buck.tv>
2018-05-30 13:43:06 -0700

    Changed ImportScript to work from the Releases Folder

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ var Config = { // eslint-disable-line no-unused-vars
         "debug"        : eval("BUILD:REPLACE{{debug}}"),
         "logMaxSize"   : 5000000,
         "resourcePath" : aeq.file.joinPath(aeq.app.getUserDataFolder().fsName, "Buck", "BUILD:REPLACE{{pkg.fullname}}"),
-
         "globalTemplatePath"  : $.getenv("BUCK_SOURCE_ROOT") ? "\\" + aeq.file.joinPath($.getenv("BUCK_SOURCE_ROOT"), "AE", "templates", "markerTemplates") : null,
-        "projectTemplatePath" : $.getenv("BUCK_PROJECT_PATH") ? "\\" + aeq.file.joinPath($.getenv("BUCK_PROJECT_PATH"), "Production", "Common", "Meta", "markerTemplates") : null
+        "projectTemplatePath" : $.getenv("BUCK_PROJECT_PATH") ? "\\" + aeq.file.joinPath($.getenv("BUCK_PROJECT_PATH"), "Production", "Common", "Meta", "markerTemplates") : null,
+        "scriptsRootPath"     : $.getenv("BUCK_SOURCE_ROOT") ? "\\" + aeq.file.joinPath($.getenv("BUCK_SOURCE_ROOT"), "AE", "scripts", "Releases") : null,
     }
 };
--- src/libs/importScript.jsx
+++ src/libs/importScript.jsx
@@ -1,40 +1,52 @@
- var ImportScript = (function (){
-    var scriptRootFolder;
-    var pcRoot = "//abadal/GlobalPrefs/work.ben/AE/Scripts";
-    var macRoot = "//GlobalPrefs/work.ben/AE/Scripts";
-      function setScriptFile(scriptPath){
-        if (isWindows === true) {
-            scriptRootFolder = Folder(pcRoot);
-          } else {
-            scriptRootFolder = Folder(macRoot);
-          }
-          var scriptFile = File(scriptRootFolder.fsName + "/" + scriptPath);
-          return scriptFile;
-          
-      }
+var ImportScript = (function() {
 
-      /**
-       * run an external script using eval. 
-       * @param {Sctring} scriptName 
-       */
-      function runScript(scriptName){  
-        var theScript = setScriptFile(scriptName);
-        if (theScript.exists) {
-            theScript.open("r");
-            var scriptToRead = theScript.read();
-            eval(scriptToRead);
-            theScript.close();
-            
-          } else {
-            alert("could not find DCtoJSON Script");
-          }
-      }
-      
-      function isWindows() {
-        return $.os.indexOf("Windows") != -1;
-      }
+  var pattern = /-v\d/;
+  var scriptRootFolder;
+  var macRoot = "//GlobalPrefs/CURRENT/src/AE/scripts/Releases";
+  var pcRoot = Config.globals.scriptsRootPath;
 
-      return {runScript:runScript}
+  function setScriptFile(scriptPath) {
+    if (isWindows === true) {
+      // var pcRootFolder = Folder(pcRoot + "/" + scriptPath);
+      findScriptFile(pcRootFolder);
+    } else {
+      var macRootFolder = Folder(macRoot + "/" + scriptPath);
+      findScriptFile(macRootFolder);
+    }
 
+    var scriptFile = File(scriptRootFolder.fsName + "/" + scriptPath + ".jsx");
+    return scriptFile;
+  }
+
+  function findScriptFile(rootFolder) {
+    var rootFolderFiles = rootFolder.getFiles();
+    for (var f = 0; f < rootFolderFiles.length; f++) {
+      if (rootFolderFiles[f].displayName.match(pattern)) {
+        scriptRootFolder = Folder(rootFolderFiles[f].fsName);
+      }
+    }
+  }
+
+  /**
+   * run an external script using eval.
+   * @param {Sctring} scriptName
+   */
+  function runScript(scriptName) {
+    var theScript = setScriptFile(scriptName);
+    if (theScript.exists) {
+      theScript.open("r");
+      var scriptToRead = theScript.read();
+      eval(scriptToRead);
+      theScript.close();
+    } else {
+      alert("could not find DCtoJSON Script");
+    }
+  }
+
+  function isWindows() {
+    return $.os.indexOf("Windows") != -1;
+  }
+
+
+  return { runScript: runScript };
 })();
-
